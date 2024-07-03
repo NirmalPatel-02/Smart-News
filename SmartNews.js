61d5a12cb213d1202e262a7ee81284f799811918
NirmalPatel-02 <nilupatel02005@gmail.com>
2024-07-03 12:06:29 +0530

    Update SmartNews.js

--- SmartNews.js
+++ SmartNews.js
@@ -1,20 +1,32 @@
 const API_KEY="9ce4d6f4b6054645af350a0f4a98ae5e";
 const url="https://newsapi.org/v2/everything?q=";
 
-window.addEventListener("load" ,() => fetchNews("India sports"));
-async function fetchNews(query){
-    const res= await fetch(`${url}${query}&apiKey=${API_KEY}`);
+let country=document.getElementById('country');
+let about=document.getElementById('about');
+
+document.getElementById("f-search").addEventListener("click" ,() => fetchNews(country.value , about.value));
+
+async function fetchNews(a,b){
+    const res= await fetch(`${url}${a + b}&apiKey=${API_KEY}`);
     const data= await res.json();
     bindData(data.articles);
 }
 function bindData(articles){
-    const main = document.getElementById('main');
-    const news_text= document.getElementById('news-text');
-    main.innerHTML ="";
+    const display = document.getElementById('display');
+    const main= document.getElementById('main');
+    const title= document.getElementById('title');
+    const detail=document.getElementById('detail');
+    const img=document.getElementById('img');
 
+    display.innerHTML="";
     articles.forEach((articles) => {
-        const clone = news_text.cloneNode(true);
-        main.appendChild(clone); 
-        news_text.innerHTML =articles.title
+        if(!articles.urlToImage){
+            return;
+        } 
+        const clone = main.cloneNode(true);
+        display.appendChild(clone); 
+        title.innerHTML =articles.title;
+        detail.innerHTML=articles.description;
+        img.src = articles.urlToImage;
     });
 }
